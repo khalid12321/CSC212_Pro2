2faa93b52ec02128926863ed30f61394a38a4dff
khalid alasmari <khalidasmari6@gmail.com>
2024-05-10 16:03:08 +0300

    add Node class

--- CSC212_P2/src/DLLImp.java
+++ CSC212_P2/src/DLLImp.java
@@ -3,62 +3,14 @@ public class DLLImp<T> implements DLL<T>{
 	
 	
 	
-	class Node<T> {
-		
-		
-		public T data;
-		public Node<T> next;
-		public Node<T> previous;
-
-		public Node () {
-			data = null;
-			next = null;
-			previous = null;
-		}
-
-		public Node (T val) {
-			data = val;
-			next = null;
-			previous= null;
-		}
-
-		public T getData() {
-			return data;
-		}
-
-		public void setData(T data) {
-			this.data = data;
-		}
-
-		public Node<T> getNext() {
-			return next;
-		}
-
-		public void setNext(Node<T> next) {
-			this.next = next;
-		}
-
-		public Node<T> getPrevious() {
-			return previous;
-		}
-
-		public void setPrevious(Node<T> previous) {
-			this.previous = previous;
-		}
-
-		
-		
-		
-		
-		
-	}
 	
 	
 	
 	
 	
 	
-	 public Node<T> head;  
+	
+	    public Node<T> head;  
 	    public Node<T> current;  
 	    private int counter;  
 	      
